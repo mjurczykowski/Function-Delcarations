1b1305be8260648b6773b60d21fc4edfc7607e99
mjurczykowski <42749593+mjurczykowski@users.noreply.github.com>
2018-09-25 13:56:29 -0500

    Add files via upload
    9/25/2018 - worked my way up to  problem 8, need to complete 8,9,10...

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Function-Delcarations
-WIP-On problem 8
+# Function Declarations
+The goal of this program is to practice using function vocabulary and write functions which alter and return a new output.  There are 10 total functions to be completed.  Type below each comment.
+
+## Math Resources
+Some of the questions require some intermediate math functions and constants.  JavaScript has stored these conveniently in a MATH LIBRARY.  You can access them by typing Math.functionName() - here is a link [Math Lib](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math) to a list of all of your options.
+
+#### Short Math list
+-  The square root function : **Math.sqrt(** input goes here **)**
+-  The number PI to many decimals : **Math.PI**
+-  How to round a number : **Math.round(** input goes here **)**
+
+## String Resources
+Do not forget that you can **glue** strings together with the plus sign **+**.  This important for inserting your variable into the string you want.  We also explored how to use string template literals briefly if you want to go that route.  Here is a link to [string template literals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals) if you do not want to use the plus sign.
---
+++ f1.js
@@ -0,0 +1,131 @@
+/*
+** This is the file f1.js
+** Read each comment below, declare each function.
+** These are all PURE FUNCTIONS, they only depend and change the input values.
+*/
+
+
+/*******************************NUMBER FOCUSED****************************/
+
+// (1) First Task
+// Declare a function named double() which outputs a number double the input number.
+
+function double(n) {
+    return n*n;
+}
+
+
+
+
+
+// (2) Second Task
+// Declare a function named plusplus() which outputs a number one higher than the input number.
+
+function plusplus(n) {
+    n+1;
+}
+
+
+
+
+
+// (3) Third Task
+// Declare a function named oppRecip() which outputs the opposite reciprocal of the input number.
+function oppRecip(n){
+    return n;
+}
+
+
+
+
+
+
+// (4) Fourth Task
+// Declare a function named pthagorean() which has two inputs you guessed it a and b.
+// The function outputs the value of the third side of the right triangle.
+
+
+function pthagorean(a,b){
+   return (a*a)+(b*b);
+}
+
+
+
+
+// (5) Fifth Task
+// Declare a function named circleArea() which has one input named radius.
+// The function outputs the area of the circle.
+
+function circleArea(radius){
+    return (radius*radius)*3.14;
+}
+
+
+
+
+
+// (6) Sixth Task
+// Declare a function named boxVolume() which has three inputs: length, width, and depth.
+// The function outputs the volume of the box;
+
+function boxVolume(length,width,depth){
+    return length*width*height;
+}
+
+
+
+
+
+/**************************** STRING FOCUSED *********************************/
+
+
+
+// (7) Seventh Task
+// Declare a function named thisIsMadness() which accepts one string input named city.
+// The function should output the string: "No! This is <city>!!"
+
+function thisIsMadness(city){
+    return 'No! This is '+city+!!;
+}
+
+
+
+
+
+// (8) Eigth Task
+// Declare a function named yo() which accepts one string input named fam.
+// The function should output the string: "yo <fam>!  What's good <fam>??"
+
+
+
+
+
+
+// (9) Ninth Task
+// Declare a function named beefDay() which has no inputs.
+// This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
+
+
+
+
+
+
+
+
+/**************************** BOOLEAN FOCUSED *******************************/
+
+
+// (10) Tenth Task
+// Declare a function named xor() which has two inputs a and b.
+// This function returns the exclusive or of the two boolean inputs.
+
+
+
+
+
+
+
+
+
+
+/**************************** END OF FILE *******************************/
