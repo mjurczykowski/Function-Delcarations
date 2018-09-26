ec70b3e6bc6167ce40fe401296c5ada868f5def3
mjurczykowski <42749593+mjurczykowski@users.noreply.github.com>
2018-09-26 13:59:09 -0500

    Add files via upload
    Still need to test

--- f1.js
+++ f1.js
@@ -10,8 +10,8 @@
 // (1) First Task
 // Declare a function named double() which outputs a number double the input number.
 
-function double(n) {
-    return n*n;
+function double(x) {
+    return x*x;
 }
 
 
@@ -21,8 +21,8 @@ function double(n) {
 // (2) Second Task
 // Declare a function named plusplus() which outputs a number one higher than the input number.
 
-function plusplus(n) {
-    n+1;
+function plusplus(x) {
+    x+1;
 }
 
 
@@ -31,8 +31,8 @@ function plusplus(n) {
 
 // (3) Third Task
 // Declare a function named oppRecip() which outputs the opposite reciprocal of the input number.
-function oppRecip(n){
-    return n;
+function oppRecip(x){
+    return x;
 }
 
 
@@ -84,9 +84,9 @@ function boxVolume(length,width,depth){
 // Declare a function named thisIsMadness() which accepts one string input named city.
 // The function should output the string: "No! This is <city>!!"
 
-function thisIsMadness(city){
-    return 'No! This is '+city+!!;
-}
+function thisIsMadness(city) {
+    return 'No! This is '+city+'!!';
+
 
 
 
@@ -96,8 +96,9 @@ function thisIsMadness(city){
 // Declare a function named yo() which accepts one string input named fam.
 // The function should output the string: "yo <fam>!  What's good <fam>??"
 
-
-
+function yo(fam) {
+    return 'yo'+fam+'! Whats good '+fam+'??';
+}
 
 
 
@@ -105,7 +106,9 @@ function thisIsMadness(city){
 // Declare a function named beefDay() which has no inputs.
 // This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
 
-
+function beefDay(){
+    return 'BEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEF DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY';
+}
 
 
 
@@ -119,13 +122,27 @@ function thisIsMadness(city){
 // Declare a function named xor() which has two inputs a and b.
 // This function returns the exclusive or of the two boolean inputs.
 
+function xor(a,b) {
+    return (a&&(!b))||(b&&(!a));
+}
+
+let x = 5;
+console.log(`For the next few examples let x = ${x}`);
+console.log(`1. double the number = ${double(x)}`);
+console.log(`2. adding one to the number = ${plusplus(x)}`);
+console.log(`3. the opposite reciprocal of ${x} is ${oppRecip(x)}`);
+console.log(`4. the pythagorean theorem does 5 = ${pythagorean(3,4)}?`);
+console.log(`5. the circle with radius ${x} has area = ${circleArea(x)}`);
+console.log(`6. the volume of a ${x}, ${2*x}, ${3*x} box has volume = ${boxVolume(x,2*x,3*x)}`);
+console.log(`7. ${thisIsMadness("SPARTA")}`);
+console.log(`8. ${yo("dude")}`);
+console.log(`9. ${beefDay()}`);
+console.log(`10. first test: true == ${xor(true,false)} == ${xor(false,true)}`);
+console.log(`10. second test: false == ${xor(true,true)} == ${xor(false,false)}`);
 
 
 
 
 
 
-
-
-
-/**************************** END OF FILE *******************************/
+/**************************** END OF FILE *******************************
