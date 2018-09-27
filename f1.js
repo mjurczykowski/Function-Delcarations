659d77e5ca89a052424df4bf415511f1cc686a8d
mjurczykowski <42749593+mjurczykowski@users.noreply.github.com>
2018-09-27 13:24:33 -0500

    Add files via upload
    Finished 9/26/18;

--- f1.js
+++ f1.js
@@ -126,21 +126,6 @@ function xor(a,b) {
     return (a&&(!b))||(b&&(!a));
 }
 
-let x = 5;
-console.log(`For the next few examples let x = ${x}`);
-console.log(`1. double the number = ${double(x)}`);
-console.log(`2. adding one to the number = ${plusplus(x)}`);
-console.log(`3. the opposite reciprocal of ${x} is ${oppRecip(x)}`);
-console.log(`4. the pythagorean theorem does 5 = ${pythagorean(3,4)}?`);
-console.log(`5. the circle with radius ${x} has area = ${circleArea(x)}`);
-console.log(`6. the volume of a ${x}, ${2*x}, ${3*x} box has volume = ${boxVolume(x,2*x,3*x)}`);
-console.log(`7. ${thisIsMadness("SPARTA")}`);
-console.log(`8. ${yo("dude")}`);
-console.log(`9. ${beefDay()}`);
-console.log(`10. first test: true == ${xor(true,false)} == ${xor(false,true)}`);
-console.log(`10. second test: false == ${xor(true,true)} == ${xor(false,false)}`);
-
-
 
 
 
